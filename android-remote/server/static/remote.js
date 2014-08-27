55c7833967c9bfae23ae0b69212258a2dc016aaf
Etienne J. Membrives <etienne@membrives.fr>
2014-08-27 21:38:45 +0200

    Bookmarlet javascript

---
+++ android-remote/server/static/remote.js
@@ -0,0 +1,42 @@
+(function(){
+
+	// the minimum version of jQuery we want
+	var vmin = "1.3";
+	var v = "1.11.1";
+
+	// check prior inclusion and version
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < vmin) {
+		var done = false;
+		var script = document.createElement("script");
+		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+				done = true;
+				initMyBookmarklet();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	} else {
+		initMyBookmarklet();
+	}
+	
+	function initMyBookmarklet() {
+		(window.myBookmarklet = function() {
+			var socket = new WebSocket("ws://127.0.0.1:6001/conn");
+			socket.onmessage = function(message) {
+				if (message.data == "a") {
+					// left
+					jQuery.event.trigger({ type : 'keypress', which : 37 });
+				} else if (message.data == "d") {
+					// right
+					jQuery.event.trigger({ type : 'keypress', which : 39 });
+				}
+			}
+			socket.onerror = function(error) {
+				console.log(error);
+			}
+		})();
+	}
+
+})();
+

85f85562361d4d47cbc2909e87589359421b8aaa
Etienne J. Membrives <etienne@membrives.fr>
2014-12-23 22:03:09 +0100

    Rewrite to use server-generated pages, part 1

--- dispotrains.webapp/app/server.go
+++ dispotrains.webapp/app/server.go
@@ -1,80 +1,92 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"html/template"
+    "log"
+	"net/http"
+
+    "github.com/sterops/tinkerings/dispotrains.webapp/storage"
+
+	"github.com/gorilla/mux"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
-	"log"
-	"net"
-	"net/http"
-	"net/http/fcgi"
-	"strings"
 )
 
-type FastCGIServer struct{}
+var (
+	homeTmpl    = template.Must(template.ParseFiles("templates/lines.html"))
+	lineTmpl    = template.Must(template.ParseFiles("templates/line.html"))
+	stationTmpl = template.Must(template.ParseFiles("templates/station.html"))
+)
 
-func (s *FastCGIServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	ServeHTTP(w, req)
+type Line struct {
+	Network  string
+	ID       string
+    GoodStations []*storage.Station `bson:"goodStations"`
+    BadStations []*storage.Station `bson:"badStations"`
 }
 
-func ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	url := req.URL
-	urlComponents := strings.Split(url.Path, "/")
-	log.Println(urlComponents)
-	if urlComponents[1] != "app" || len(urlComponents) < 3 {
-		log.Println("Not in app:", urlComponents)
-		return
+type LineHolder struct {
+	Value Line
+}
+
+type LineSlice []LineHolder
+
+func (ls LineSlice) Lines() []Line {
+	r := make([]Line, len(ls))
+	for i, v := range ls {
+		r[i] = v.Value
 	}
+	return r
+}
+
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
 	session, err := mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
-
-	// Optional. Switch the session to a monotonic behavior
-	session.SetMode(mgo.Monotonic, true)
-
-	if urlComponents[2] == "GetLines" {
-		c := session.DB("dispotrains").C("lines")
-		var lines []bson.M = make([]bson.M, 0)
-		c.Find(nil).Sort("value.network", "value.id").All(&lines)
-		jsonLines := make([]bson.M, 0)
-		for _, line := range lines {
-			jsonLines = append(jsonLines, line["value"].(bson.M))
-		}
-		json.NewEncoder(w).Encode(&jsonLines)
-	} else if urlComponents[2] == "GetStation" && len(urlComponents) >= 4 {
-		var stationName string = urlComponents[3]
-		c := session.DB("dispotrains").C("stations")
-		var station bson.M
-		c.Find(bson.M{"name": stationName}).One(&station)
-		json.NewEncoder(w).Encode(&station)
-	} else if urlComponents[2] == "GetStations" {
-		c := session.DB("dispotrains").C("stations")
-		var stations []bson.M = make([]bson.M, 0)
-		if err := c.Find(nil).All(&stations); err != nil {
-			log.Println(err)
-		}
-		jsonStations := make([]bson.M, 0)
-		for _, station := range stations {
-			delete(station, "_id")
-			jsonStations = append(jsonStations, station)
-		}
-		json.NewEncoder(w).Encode(&jsonStations)
-	} else {
-		log.Println("Not a recognized action")
-	}
+	c := session.DB("dispotrains").C("lines")
+	var lines LineSlice = make(LineSlice, 0)
+	c.Find(nil).Sort("value.network", "value.id").All(&lines)
+	homeTmpl.Execute(w, lines.Lines())
 }
 
-/*func main() {
-    http.HandleFunc("/", ServeHTTP)
-    http.ListenAndServe(":8080", nil)
-}*/
+func LineHandler(w http.ResponseWriter, req *http.Request) {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	c := session.DB("dispotrains").C("lines")
+
+    vars := mux.Vars(req)
+    lineId := vars["line"]
+
+	var line LineHolder
+    c.Find(bson.M{"_id": lineId}).One(&line)
+	if err = lineTmpl.Execute(w, line.Value); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func StationHandler(w http.ResponseWriter, req *http.Request) {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	c := session.DB("dispotrains").C("lines")
+	var lines LineSlice = make(LineSlice, 0)
+	c.Find(nil).Sort("value.network", "value.id").All(&lines)
+	homeTmpl.Execute(w, lines.Lines())
+}
 
 func main() {
-	fmt.Printf("Starting server")
-	l, _ := net.Listen("tcp", "127.0.0.1:9000")
-	b := new(FastCGIServer)
-	fcgi.Serve(l, b)
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/ligne/{line}", LineHandler)
+	r.HandleFunc("/gare/{station}", StationHandler)
+    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/", r)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }

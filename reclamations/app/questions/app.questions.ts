b4942caaad89ec2842b1d3869cbf8fbe7d788708
Etienne J. Membrives <etienne@membrives.fr>
2016-10-23 21:54:03 +0200

    RadioQuestion

--- reclamations/app/questions/app.questions.ts
+++ reclamations/app/questions/app.questions.ts
@@ -4,3 +4,24 @@ export function createEngine(): Engine {
   let e = new Engine();
   return e;
 }
+
+class RadioQuestion implements Question {
+  private variable: string;
+  constructor(template: string, priority: number, variable: string) {
+    this.template = template;
+    this.priority = priority;
+    this.variable = variable;
+  }
+
+  isSet(): boolean {
+    return false;
+  }
+
+  getValues() : Value[] {
+    return [];
+  }
+
+  isActive(s: State) : boolean {
+    return true;
+  }
+}
